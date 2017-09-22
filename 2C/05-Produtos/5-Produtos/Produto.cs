4e29412cf71ba125596a6dcd3f589120241f2bf2
barbara <capriglioneeee@hotmail.com>
2017-09-22 17:44:01 -0300

    ex 5

---
+++ 2C/05-Produtos/5-Produtos/Produto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Produtos
+{
+    class Produto
+    {       
+        private int id;
+        public int Id{ get { return id; } }
+
+        private string nome;
+        public string Nome { get { return nome; } }
+
+        private double preco;
+        public double Preco { get { return preco; } }
+
+        private int quantidade;
+        public int Quantidade { get { return quantidade; } }
+        public Produto(int id, string nome, double preco)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.preco = preco;
+        }
+        public void Reposicao(int quantidade)
+        {
+            this.quantidade += quantidade;           
+        }
+        public void Retirada(int qtd)
+        {
+            if (this.Quantidade >= qtd) {this.quantidade -= qtd;}
+            else
+                throw new Exception();
+        }
+        public string Imprimir()
+        {
+            return string.Format("{0}, {1}, {2:0.00}", id, nome, preco);
+        }
+ 
+    }
+}
