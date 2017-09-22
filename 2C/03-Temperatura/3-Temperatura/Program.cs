d4b1a265c8e765295aa44f604b86dd78b5bfc02a
barbara <capriglioneeee@hotmail.com>
2017-09-22 17:28:02 -0300

    exercicio temperatura

---
+++ 2C/03-Temperatura/3-Temperatura/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Temperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperaturaCelsius obj = new TemperaturaCelsius();
+            obj.temp = 0;
+            obj.temp = double.Parse(Console.ReadLine());
+            double receberF = obj.ParaFahrenheit();            
+            double receberK = obj.ParaKelvin();
+            Console.WriteLine("{0}ºF e {1}K",receberF,receberK);
+        }
+    }
+}
---
+++ 2C/03-Temperatura/3-Temperatura/TemperaturaCelsius.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Temperatura
+{
+    class TemperaturaCelsius
+    {
+        public double temp;
+
+        public double ParaFahrenheit() 
+        {
+            double f = (9*temp + 160)/5;
+            return f;
+        }  
+
+        public double ParaKelvin()
+        {
+            double k = temp + 273;
+            return k;
+        }
+    }    
+}
